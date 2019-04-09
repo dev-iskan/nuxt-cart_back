8158266cd594d896cb0ae7b08bdd42bb29f1b048
dev-iskan <successfuliskan@gmail.com>
2019-04-09 22:15:08 +0500

    fixed code with php cs fixer

--- app/Http/Controllers/Countries/CountryController.php
+++ app/Http/Controllers/Countries/CountryController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Countries;
 
 use App\Http\Resources\CountryResource;
 use App\Models\Country;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class CountryController extends Controller
